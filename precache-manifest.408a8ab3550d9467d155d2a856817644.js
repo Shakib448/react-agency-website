4fab50756dbbe25c804859b5fdfb92a4c37f6dd2
Shakib448 <shakiba448@gmail.com>
2020-08-14 10:25:16 +0600

    Updates

---
+++ precache-manifest.408a8ab3550d9467d155d2a856817644.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa54d2acb899e576f2dd2f57438a1e18",
+    "url": "/react-agency-website/index.html"
+  },
+  {
+    "revision": "160e0bcfc97b90b76952",
+    "url": "/react-agency-website/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "01ac3b58c5f4e1c68683",
+    "url": "/react-agency-website/static/js/2.a92f600b.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/react-agency-website/static/js/2.a92f600b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "160e0bcfc97b90b76952",
+    "url": "/react-agency-website/static/js/main.cc82a1ee.chunk.js"
+  },
+  {
+    "revision": "20c469cf33d69da28fb1",
+    "url": "/react-agency-website/static/js/runtime-main.9b8eb7fe.js"
+  },
+  {
+    "revision": "eda4598198a00832fb334d5a4e588ead",
+    "url": "/react-agency-website/static/media/01-thumbnail.eda45981.jpg"
+  },
+  {
+    "revision": "e52270b3af9b353d02078b01acd931ae",
+    "url": "/react-agency-website/static/media/02-thumbnail.e52270b3.jpg"
+  },
+  {
+    "revision": "09c41872ba6c96c6efdcab7f202a71a7",
+    "url": "/react-agency-website/static/media/03-thumbnail.09c41872.jpg"
+  },
+  {
+    "revision": "80c8ff5c4a1a0775616a5541086b1d51",
+    "url": "/react-agency-website/static/media/04-thumbnail.80c8ff5c.jpg"
+  },
+  {
+    "revision": "68d8d9495f59fa5109f3b337d784cacc",
+    "url": "/react-agency-website/static/media/05-thumbnail.68d8d949.jpg"
+  },
+  {
+    "revision": "20456fff3658b40a59a73ae9821a273f",
+    "url": "/react-agency-website/static/media/06-thumbnail.20456fff.jpg"
+  },
+  {
+    "revision": "e0b34f6fc62c82231d2016e7e85d7ed0",
+    "url": "/react-agency-website/static/media/1.e0b34f6f.jpg"
+  },
+  {
+    "revision": "a90b0f86f471b7840c8c3b678620cbf3",
+    "url": "/react-agency-website/static/media/2.a90b0f86.jpg"
+  },
+  {
+    "revision": "cc41e47b8fbdb1c81c375b65071e478c",
+    "url": "/react-agency-website/static/media/2.cc41e47b.jpg"
+  },
+  {
+    "revision": "2df43e3546ec0d6666badb2ddbc716ee",
+    "url": "/react-agency-website/static/media/3.2df43e35.jpg"
+  },
+  {
+    "revision": "e2d2a51c2932205b52f4a5ae1943eb97",
+    "url": "/react-agency-website/static/media/header-bg.e2d2a51c.jpg"
+  }
+]);
